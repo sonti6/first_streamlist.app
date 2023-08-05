4f84111d2d1357535e95e1419bee77ecf1212609
Ravi Sonti <64358314+sonti6@users.noreply.github.com>
2023-08-05 14:25:32 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,14 +11,19 @@ streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-# Let's put a pick list here so they can pick the fruit they want to include 
-fruits_to_show= streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avacado', 'Strawberries'])
-fruits_to_show = my_fruit_list.loc[fruits_selected]
+
 
 # Display the table on the page.
 my_fruit_list = my_fruit_list.set_index('Fruit')
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
+
+
+# Let's put a pick list here so they can pick the fruit they want to include 
+fruits_to_show= streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avacado', 'Strawberries'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
+
+
 
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
