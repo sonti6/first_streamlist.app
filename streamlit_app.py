ce98687d395dde0377bbc673ef8ef8b9adbc656a
Ravi Sonti <64358314+sonti6@users.noreply.github.com>
2023-08-05 14:23:56 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 my_fruit_list = my_fruit_list.set_index('Fruit')
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.dataframe(fruits_to_show)
+streamlit.dataframe(my_fruit_list)
 
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
